333f32f20deedc545ffc9d5ba89f445bb832fd8e
eromanova <eromanova@molnet.ru>
2022-04-04 19:03:30 +0300

    Add extra functionality to script_03.py

--- script_03.py
+++ script_03.py
@@ -1,9 +1,9 @@
 import requests
+for i in range(1, 5):
+    output_filename = f"unsplash_image_{i}.jpeg"
+    BASE_URL = "https://source.unsplash.com/random"
 
-output_filename = "unsplash_image.jpeg"
-BASE_URL = "https://source.unsplash.com/random"
-
-response = requests.get(BASE_URL)
-print(response.ok)
-with open(output_filename, "wb") as f:
-    f.write(response.content)
+    response = requests.get(BASE_URL)
+    print(response.ok)
+    with open(output_filename, "wb") as f:
+        f.write(response.content)
